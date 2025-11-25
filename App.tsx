430a37066817e6e70a0bd651b0f92f6a9089df24
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-25 09:33:19 -0300

    Application assets

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { colors } from "@/theme/colors";
 
 export default function App() {
   return (
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.white,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
