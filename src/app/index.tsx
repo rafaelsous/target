d63a92c3ed2f49b6958a5a38568c313235f6e7b0
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-25 11:08:31 -0300

    Creating the route In Progress

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -16,8 +16,14 @@ export default function Index() {
       <Text>Olá, Expo Router!</Text>
 
       <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+
       <Button
         title="Transação"
+        onPress={() => router.navigate("/transaction/1207")}
+      />
+
+      <Button
+        title="Progresso"
         onPress={() => router.navigate("/transaction/1207")}
       />
     </View>
