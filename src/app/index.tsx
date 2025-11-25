3599c5f7ef7a1c5971eb01b0f19e7232c6b6437e
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-25 10:51:22 -0300

    Browsing with the Expo Router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+import { colors } from "@/theme/colors";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: colors.white,
+      }}
+    >
       <Text>Olá, Expo Router!</Text>
+
+      <Button title="Nova meta" onPress={() => router.navigate("/target")} />
     </View>
   );
 }
