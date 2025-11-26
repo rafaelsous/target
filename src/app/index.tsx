ede79a4764f83b57bc554ce30d1319bb4b3a3dad
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-25 23:17:39 -0300

    Target component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,37 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
 
 const summary = {
   total: "R$ 22.549,76",
   input: { label: "Entradas", value: "R$ 11.788,18" },
   output: { label: "Saídas", value: "-R$ 2.377,83" },
 };
+
+const targets = [
+  {
+    id: Math.random().toString(36).substring(2),
+    name: "Apple Watch",
+    percentage: "50%",
+    current: "R$ 895,00",
+    target: "R$ 1.790,00",
+  },
+  {
+    id: Math.random().toString(36).substring(2),
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "R$ 900,00",
+    target: "R$ 1.200,00",
+  },
+  {
+    id: Math.random().toString(36).substring(2),
+    name: "Fazer uma viagem para o Rio de Janeiro",
+    percentage: "75%",
+    current: "R$ 900,00",
+    target: "R$ 2.250,00",
+  },
+];
 
 export default function Index() {
   return (
@@ -16,6 +41,10 @@ export default function Index() {
       }}
     >
       <HomeHeader data={summary} />
+
+      {targets.map((target) => (
+        <Target key={target.id} data={target} />
+      ))}
     </View>
   );
 }
---
+++ src/components/Target/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+import { colors, fontFamily } from "@/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  content: {
+    flex: 1,
+    gap: 7,
+  },
+  name: {
+    fontFamily: fontFamily.medium,
+    fontSize: 14,
+    color: colors.black,
+  },
+  status: {
+    fontFamily: fontFamily.regular,
+    fontSize: 10,
+    color: colors.gray[600],
+  },
+});
