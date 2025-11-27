d7f756086dcd7cfdeaa7807eeccb8bc0e1ae4f5b
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-27 19:12:44 -0300

    Creating transactions table

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -2,7 +2,7 @@ import { type SQLiteDatabase } from "expo-sqlite";
 
 export async function migrate(database: SQLiteDatabase) {
   await database.execAsync(`
-    PRAGMA foreign_key = ON;
+    PRAGMA foreign_keys = ON;
 
     CREATE TABLE IF NOT EXISTS targets (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,6 +10,19 @@ export async function migrate(database: SQLiteDatabase) {
       amount FLOAT NOT NULL,
       created_at timestamp NOT NULL DEFAULT current_timestamp,
       updated_at timestamp NOT NULL DEFAULT current_timestamp
+    );
+
+    CREATE TABLE IF NOT EXISTS transactions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      target_id INTEGER NOT NULL,
+      amout FLOAT NOT NULL,
+      observation TEXT NULL,
+      created_at timestamp NOT NULL DEFAULT current_timestamp,
+      updated_at timestamp NOT NULL DEFAULT current_timestamp,
+
+      CONSTRAINT fk_targets_transactions
+      FOREIGN KEY (target_id) REFERENCES targets(id)
+      ON DELETE CASCADE
     );
   `);
 }
