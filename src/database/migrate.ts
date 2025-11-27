349935621f2a2d457305659d11fa56d05edb7c9d
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-27 19:07:48 -0300

    Creating targets table

---
+++ src/database/migrate.ts
@@ -0,0 +1,15 @@
+import { type SQLiteDatabase } from "expo-sqlite";
+
+export async function migrate(database: SQLiteDatabase) {
+  await database.execAsync(`
+    PRAGMA foreign_key = ON;
+
+    CREATE TABLE IF NOT EXISTS targets (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      amount FLOAT NOT NULL,
+      created_at timestamp NOT NULL DEFAULT current_timestamp,
+      updated_at timestamp NOT NULL DEFAULT current_timestamp
+    );
+  `);
+}
