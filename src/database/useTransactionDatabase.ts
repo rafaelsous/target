0379782820aeea49b2465ebbf47967e776ee4032
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-28 15:37:21 -0300

    Method of recording a transaction in the database

---
+++ src/database/useTransactionDatabase.ts
@@ -0,0 +1,30 @@
+import { useSQLiteContext } from "expo-sqlite";
+
+export type TransactionCreate = {
+  target_id: number;
+  amount: number;
+  observation?: string;
+};
+
+export function useTransactionDatabase() {
+  const database = useSQLiteContext();
+
+  async function create(data: TransactionCreate) {
+    const statement = await database.prepareAsync(`
+      INSERT INTO transactions 
+        (target_id, amount, observation) 
+      VALUES 
+        ($target_id, $amount, $observation)
+    `);
+
+    statement.executeAsync({
+      $target_id: data.target_id,
+      $amount: data.amount,
+      $observation: data.observation ? data.observation : null,
+    });
+  }
+
+  return {
+    create,
+  };
+}
